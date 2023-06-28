9a830f68d3bb6f848f747b5b6a5e4c74c286a9be
Jeffrey Walton <noloader@gmail.com>
2023-06-28 17:54:16 -0400

    Update make-book.sh script

--- make-book.sh
+++ make-book.sh
@@ -5,13 +5,6 @@
 # Name without extensions. The final artifact includes the PDF extension.
 BOOKNAME=docbook-install
 
-# This works around a bug in FOP. fonts.xml says the fonts base
-# directory is ../, but FOP can only consume ./.
-if [[ ! -d ./fonts/ ]]; then
-    echo "Copying fonts..."
-    cp -r ../fonts ./
-fi
-
 if [[ ! "$(command -v xmllint 2>/dev/null)" ]]
 then
     echo "xmllint is not installed. Skipping validation."
